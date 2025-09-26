0a0f3b5b40d8e54e7c6420b6aabe79a308d0fe9c
CharlesOsang017 <charlesosango02@gmail.com>
2025-09-26 17:22:07 +0300

    Add user register controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,11 +1,24 @@
-export const createAdminUser = async(req, res)=>{
-    try {
-        const {name, email, password} = req.body;
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
-        
-
-    } catch (error) {
-        console.log('error creating admin user', error.message)
-        return res.status(500).json({message: error.message})
+export const createAdminUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    // check for existing user
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
-}
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // create a new user
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.log("error creating admin user", error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { validateRequest } from "zod-express-middleware";
 import { registerAdminSchema } from "../libs/validate-schema.js";
+import { createAdminUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
