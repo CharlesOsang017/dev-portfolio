dfa7fad6cfbc10b5da8de8a485772dfa6b5e8ed9
CharlesOsang017 <charlesosango02@gmail.com>
2025-09-26 20:05:50 +0300

    Add delete experience controller

--- backend/controllers/experience.controller.js
+++ backend/controllers/experience.controller.js
@@ -1,29 +1,40 @@
 import Experience from "../models/experience.model.js";
 
-export const createExperience = async(req, res)=>{
-    try {
-        const { title, company, startDate, endDate, description } = req.body;
-        const newExperience = await Experience.create({
-            title,
-            company,
-            startDate,
-            endDate,
-            description
-        })
-        await newExperience.save();
-        return res.status(201).json({ message: "Experience created successfully" });
-    } catch (error) {
-        console.log("error creating experience", error.message);
-        return res.status(500).json({ message: error.message });
-    }
-}
+export const createExperience = async (req, res) => {
+  try {
+    const { title, company, startDate, endDate, description } = req.body;
+    const newExperience = await Experience.create({
+      title,
+      company,
+      startDate,
+      endDate,
+      description,
+    });
+    await newExperience.save();
+    return res.status(201).json({ message: "Experience created successfully" });
+  } catch (error) {
+    console.log("error creating experience", error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-export const allExperience = async(req, res)=>{
-    try {
-        const allExperience = await Experience.find({});
-        return res.status(200).json(allExperience);
-    } catch (error) {
-        console.log("error getting all experience", error.message);
-        return res.status(500).json({ message: error.message });
-    }
-}
+export const allExperience = async (req, res) => {
+  try {
+    const allExperience = await Experience.find({});
+    return res.status(200).json(allExperience);
+  } catch (error) {
+    console.log("error getting all experience", error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteExperience = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Experience.findByIdAndDelete(id);
+    return res.status(200).json({ message: "Experience deleted successfully" });
+  } catch (error) {
+    console.log("error deleting experience", error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
--- backend/routes/experience.route.js
+++ backend/routes/experience.route.js
@@ -2,7 +2,12 @@ import express from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 import { validateRequest } from "zod-express-middleware";
 import { experienceSchema } from "../libs/validate-schema.js";
-import { createExperience, allExperience } from "../controllers/experience.controller.js";
+import {
+  createExperience,
+  allExperience,
+  deleteExperience,
+} from "../controllers/experience.controller.js";
+import { z } from "zod";
 
 const router = express.Router();
 
@@ -13,5 +18,11 @@ router.post(
   createExperience
 );
 router.get("/", allExperience);
+router.delete(
+  "/:id",
+  authMiddleware,
+  validateRequest({ params: z.object({ id: z.string() }) }),
+  deleteExperience
+);
 
 export default router;
